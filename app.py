a551e8f189fdbc495ad8d101d95f1098cef22a57
projetospyton2025 <marciofernando.maia@yahoo.com.br>
2025-02-18 09:29:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,12 @@ def get_ultimo_concurso():
     except Exception as e:
         return jsonify({"error": str(e)}), 500       
 
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+    
+"""    
 if __name__ == '__main__':
     app.run(debug=True)
+"""
